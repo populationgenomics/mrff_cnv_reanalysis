afcb2f4987a9bd598cebd60d29f8ccb97b883cdd
MattWellie <mattwellie@gmail.com>
2023-07-11 11:28:38 +1000

    docstring

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+
 """
 just a quick test script
+
+set this off using
+analysis-runner \
+    --dataset acute-care \
+    --access-level test \
+    --description "running a test" \
+    --output-dir "test_script" \
+    scripts/test.py
 """
 
 print('hello, World!')
